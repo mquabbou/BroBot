1a2d3ad13cc1a3a307dac17ddfcf6780d0d279ca
mquabbou <m.quabbou@gmail.com>
2021-10-07 13:47:26 +0100

    added playmusic by name

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 import random
 import youtube_dl
 import os
+from youtubesearchpython import VideosSearch
+
+playlist = []
 
 #ssh 
 
@@ -29,19 +32,40 @@ async def leave(ctx):
     await ctx.voice_client.disconnect()
     await ctx.send(f'Thalla akhay bro')
 
+@client.command(name="skip")
+async def play(ctx,url):
+
+    await ctx.send("skipit xD")
+
+
 
 @client.command(name="play")
 async def play(ctx,url):
+
+        playlist.append(url)
+
+
         ctx.voice_client.stop()
         FFMPEG_OPTIONS={'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
         YDL_OPTIONS = {'format': "bestaudio"}
         vc = ctx.voice_client
 
+
         with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            if "youtube.com" not in url:
+        
+                videosSearch = VideosSearch(url, limit = 1)
+
+                vid_link = videosSearch.result()["result"][0]['id']
+                url = "https://www.youtube.com/watch?v=" + vid_link
+                print(url)
+            
             info = ydl.extract_info(url,download=False)
             url2 = info['formats'][0]['url']
             source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
             vc.play(source)
+
+
 
 @client.command()
 async def stop(ctx):
